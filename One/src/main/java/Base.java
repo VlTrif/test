3eef62751e42b438226084358c8e0d21096a955d
Vladimir <trifonov.vladimir@physics.msu.ru>
2017-12-07 15:31:13 +0300

    Вторая задача

--- One/src/main/java/Base.java
+++ One/src/main/java/Base.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+/**
+ *@author Trifonov Vladimir
+ */
+
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input first number : ");
+        float a = scan.nextFloat(); // Ввод первого числа
+
+        System.out.print("Input second number : ");
+        float b = scan.nextFloat(); // Ввод второго числа
+
+        scan.close();
+
+        System.out.print(" Sum = ");
+        System.out.printf("%.4f", a + b); // Так как 4 знака после точки
     }
 }
